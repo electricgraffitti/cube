2927a921f7c35d13b345dd8e4a2d0d1acc303101
Larry FInn <Cube1@larry-finns-macbook-pro.local>
2010-02-05 12:44:43 -0800

    banner partial blog stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @blogs = Blog.small_list(1).last_created
-    @employees = Employee.all
+    @employees = Employee.last_created
     @code_blogs = CodeBlog.small_list(2).last_created
     @services = Service.small_list(4).last_created
    
