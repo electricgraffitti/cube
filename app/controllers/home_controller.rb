3e47dd20d77ab5227b589bd1ab98a7b6da9e4889
Larry FInn <Cube1@larry-finns-macbook-pro.local>
2010-02-05 14:49:46 -0800

    order change on services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @blogs = Blog.small_list(1).last_created
     @employees = Employee.last_created
     @code_blogs = CodeBlog.small_list(2).last_created
-    @services = Service.small_list(4).last_created
+    @services = Service.small_list(3)
    
   end
 
