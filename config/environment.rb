728ce7e405a888c555e2d6296e7156f4d827f996
Bob Hanson <Bob@super17.local>
2010-02-06 17:10:26 -0800

    added 3d clients

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "tmail"
   config.gem 'paperclip', :source => 'http://gemcutter.org'
   config.gem "openrain-action_mailer_tls", :lib => "smtp_tls.rb", :source => "http://gems.github.com"
+  config.gem 'RedCloth', :version => ">= 4.1.1", :source => "http://code.whytheluckystiff.net"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
