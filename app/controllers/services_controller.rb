a8e869eeb17180ab3f961353c2d49ca8a7c3f0f1
Larry FInn <Cube1@larry-finns-macbook-pro.local>
2010-02-09 10:55:47 -0800

    index change

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,7 +7,7 @@ class ServicesController < ApplicationController
   # GET /services
   # GET /services.xml
   def index
-    @services = Service.last_created
+    @services = Service.all
 
     respond_to do |format|
       format.html # index.html.erb
