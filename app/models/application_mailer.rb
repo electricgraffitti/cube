2ec1a8debb63d6c3c35032b7675d44b2bbe3a4b7
Bob Hanson <Bob@super17.local>
2010-02-10 17:33:25 -0800

    mailer stuff

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   
   def contact_mailer(params, sent_at = Time.now)
-      recipients    "bob@cube2media.com"
+      recipients    "support@cube2media.com"
       from          "Cube2 Contact Mailer"
       subject       "Cube2 Contact Submission"
       body          :contact => params, :sent_on => sent_at
