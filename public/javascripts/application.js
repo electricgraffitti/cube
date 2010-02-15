549b5475daf8d19d3713bde93322854338023956
Bob Hanson <Bob@super17.local>
2010-02-14 18:56:13 -0800

    added the YUI RTE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -151,38 +151,72 @@ $(document).ready(function() {
 // 		]
 // 	});
 // });
-markupSettings = {
-    nameSpace:       "html", // Useful to prevent multi-instances CSS conflict
-    onShiftEnter:    {keepDefault:false, replaceWith:'<br />\n'},
-    onCtrlEnter:     {keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
-    onTab:           {keepDefault:false, openWith:'     '},
-    markupSet:  [
-        {name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
-        {name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
-        {name:'Heading 3', key:'3', openWith:'<h3(!( class="[![Class]!]")!)>', closeWith:'</h3>', placeHolder:'Your title here...' },
-        {name:'Heading 4', key:'4', openWith:'<h4(!( class="[![Class]!]")!)>', closeWith:'</h4>', placeHolder:'Your title here...' },
-        {name:'Heading 5', key:'5', openWith:'<h5(!( class="[![Class]!]")!)>', closeWith:'</h5>', placeHolder:'Your title here...' },
-        {name:'Heading 6', key:'6', openWith:'<h6(!( class="[![Class]!]")!)>', closeWith:'</h6>', placeHolder:'Your title here...' },
-        {name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>'  },
-        {separator:'---------------' },
-        {name:'Bold', key:'B', openWith:'<strong>', closeWith:'</strong>' },
-        {name:'Italic', key:'I', openWith:'<em>', closeWith:'</em>'  },
-        {name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
-        {separator:'---------------' },
-        {name:'Ul', openWith:'<ul>\n', closeWith:'</ul>\n' },
-        {name:'Ol', openWith:'<ol>\n', closeWith:'</ol>\n' },
-        {name:'Li', openWith:'<li>', closeWith:'</li>' },
-        {separator:'---------------' },
-        {name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
-        {name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
-        {separator:'---------------' },
-        {name:'Clean', replaceWith:function(h) { return h.selection.replace(/<(.*?)>/g, "") } },
-        {name:'Preview', call:'preview', className:'preview' }
-    ]
-}
-// Markitup
-$(document).ready(function() {
-	$("#code_blog_content").markItUp(markupSettings);
+// markupSettings = {
+//     nameSpace:       "html", // Useful to prevent multi-instances CSS conflict
+//     onShiftEnter:    {keepDefault:false, replaceWith:'<br />\n'},
+//     onCtrlEnter:     {keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
+//     onTab:           {keepDefault:false, openWith:'     '},
+//     markupSet:  [
+//         {name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
+//         {name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
+//         {name:'Heading 3', key:'3', openWith:'<h3(!( class="[![Class]!]")!)>', closeWith:'</h3>', placeHolder:'Your title here...' },
+//         {name:'Heading 4', key:'4', openWith:'<h4(!( class="[![Class]!]")!)>', closeWith:'</h4>', placeHolder:'Your title here...' },
+//         {name:'Heading 5', key:'5', openWith:'<h5(!( class="[![Class]!]")!)>', closeWith:'</h5>', placeHolder:'Your title here...' },
+//         {name:'Heading 6', key:'6', openWith:'<h6(!( class="[![Class]!]")!)>', closeWith:'</h6>', placeHolder:'Your title here...' },
+//         {name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>'  },
+//         {separator:'---------------' },
+//         {name:'Bold', key:'B', openWith:'<strong>', closeWith:'</strong>' },
+//         {name:'Italic', key:'I', openWith:'<em>', closeWith:'</em>'  },
+//         {name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
+//         {separator:'---------------' },
+//         {name:'Ul', openWith:'<ul>\n', closeWith:'</ul>\n' },
+//         {name:'Ol', openWith:'<ol>\n', closeWith:'</ol>\n' },
+//         {name:'Li', openWith:'<li>', closeWith:'</li>' },
+//         {separator:'---------------' },
+//         {name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
+//         {name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
+//         {separator:'---------------' },
+//         {name:'Clean', replaceWith:function(h) { return h.selection.replace(/<(.*?)>/g, "") } },
+//         {name:'Preview', call:'preview', className:'preview' }
+//     ]
+// }
+// // Markitup
+// $(document).ready(function() {
+// 	$("#code_blog_content").markItUp(markupSettings);
+// });
+
+var codeBlog = new YAHOO.widget.Editor('code_blog_content', {
+    height: '300px',
+    width: '715px',
+    dompath: true, //Turns on the bar at the bottom
+    animate: true //Animates the opening, closing and moving of Editor windows
+});
+codeBlog.render();
+
+//Inside an event handler after the Editor is rendered
+YAHOO.util.Event.on('code_blog_submit', 'click', function() {
+    //Put the HTML back into the text area
+    codeBlog.saveHTML();
+ 
+    //The var html will now have the contents of the textarea
+    var html = codeBlog.get('code_blog_content').value;
+});
+
+var cubeBlog = new YAHOO.widget.Editor('blog_content', {
+    height: '300px',
+    width: '715px',
+    dompath: true, //Turns on the bar at the bottom
+    animate: true //Animates the opening, closing and moving of Editor windows
+});
+cubeBlog.render();
+
+//Inside an event handler after the Editor is rendered
+YAHOO.util.Event.on('blog_submit', 'click', function() {
+    //Put the HTML back into the text area
+    cubeBlog.saveHTML();
+ 
+    //The var html will now have the contents of the textarea
+    var html = cubeBlog.get('blog_content').value;
 });
 
 // Sets up the return tweets in the footer
