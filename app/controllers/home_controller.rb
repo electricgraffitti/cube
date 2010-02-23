759f6ee4117bd98da47b15c12c7c16e817a935d5
Larry FInn <Cube1@larry-finns-macbook-pro.local>
2010-02-22 18:45:41 -0800

    afterthoughts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @employees = Employee.last_created
-    @code_blogs = CodeBlog.small_list(2).last_created
+    @code_blog = CodeBlog.last
     @services = Service.small_list(3).indexed_order
   end
 
