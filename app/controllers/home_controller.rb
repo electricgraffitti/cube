576591eb0ff107f7db025225f0edbe9b918546c3
Larry FInn <Cube1@larry-finns-macbook-pro.local>
2010-02-22 19:20:19 -0800

    fixed index blog

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def index
     @employees = Employee.last_created
-    @code_blog = CodeBlog.last
+    @blog = Blog.last
     @services = Service.small_list(3).indexed_order
   end
 
