16f0e2d6e1708c524f162102e499b48e9c11ebc6
Bob <bob@electricgraffitti.com>
2010-03-05 10:01:28 -0700

    added phone number field and basic JS validations

--- public/javascripts/cube_validations.js
+++ public/javascripts/cube_validations.js
@@ -1,18 +1,20 @@
 // JS validations go here
 
-// $(document).ready(function() {
-//   
-//   // Validates a Subscription
-//     $('#new_subscription').validate({
-//       rules: {
-//         "subscription[name]": "required",
-//         "subscription[email]": {required: true, email: true}
-//       },
-//       messages: {
-//         "subscription[name]": "Please enter your name",
-//         "subscription[email]": {
-//           required: "Please enter an email address", 
-//           email: "your email is not valid."}
-//       }
-//     });
-// });
+$(document).ready(function() {
+  
+  // Validates a Subscription
+    $('#contact_form').validate({
+      rules: {
+        "contact_first_name": "required",
+        "contact_last_name": "required",
+        "contact_email": {required: true, email: true}
+      },
+      messages: {
+        "contact_first_name": "First name required",
+        "contact_last_name": "Last name required",
+        "contact_last_name": {
+          required: "Email required", 
+          email: "not a valid email address"}
+      }
+    });
+});
