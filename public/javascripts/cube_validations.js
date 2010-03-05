d991dee70c59532e0332ce810ff1e516d6237c8f
Bob <bob@electricgraffitti.com>
2010-03-05 10:04:34 -0700

    added notes

--- public/javascripts/cube_validations.js
+++ public/javascripts/cube_validations.js
@@ -3,14 +3,14 @@
 $(document).ready(function() {
   
   // Validates a Subscription
-    $('#contact_form').validate({
+    $('#contact_form').validate({ // The #name is the ID of the form element/tag
       rules: {
-        "contact_first_name": "required",
+        "contact_first_name": "required", // The first half is the value of the NAME attribute of the input tag.
         "contact_last_name": "required",
         "contact_email": {required: true, email: true}
       },
       messages: {
-        "contact_first_name": "First name required",
+        "contact_first_name": "First name required", // again, the NAME attribute, and then the value of the message if it fails validation
         "contact_last_name": "Last name required",
         "contact_last_name": {
           required: "Email required", 
