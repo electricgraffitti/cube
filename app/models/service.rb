a59aaf38ea4eb6718f475a93d33f924ba5a866a7
Bob Hanson <Bob@super17.local>
2011-03-11 21:03:00 -0700

    turned back on the RedCloth

--- app/models/service.rb
+++ app/models/service.rb
@@ -24,7 +24,7 @@ class Service < ActiveRecord::Base
    validates_presence_of :title, :sub_title, :content
 
    #redcloth
-   #acts_as_textiled :content
+   acts_as_textiled :content
 
    #named scopes
    named_scope :small_list, lambda { |limit| {:include => [:assets], :limit => limit}}
--- config/environment.rb
+++ config/environment.rb
@@ -23,10 +23,10 @@ Rails::Initializer.run do |config|
   config.gem "tmail"
   config.gem 'paperclip', :source => 'http://gemcutter.org'
   config.gem "openrain-action_mailer_tls", :lib => "smtp_tls.rb", :source => "http://gems.github.com"
-  # config.gem "tiny_mce"
-  # config.gem 'RedCloth'
-  # config.gem 'will_paginate', :version  => '~> 2.3.15', :source => 'http://gemcutter.org'
-  config.gem 'will_paginate'
+  config.gem "tiny_mce"
+  config.gem 'RedCloth'
+  config.gem 'will_paginate', :version  => '~> 2.3.15', :source => 'http://gemcutter.org'
+  # config.gem 'will_paginate'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
