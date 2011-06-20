87112ca1a3aa16ff78846b47d1c0b4c4df5e0f33
Larry FInn <Cube1@larry-finns-macbook-pro-3.local>
2011-06-19 21:19:02 -0700

    updated the email addys in the mailers fixed the quote mailer not working right added styles to both form pages added project type to quote form

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -1,14 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   
   def contact_mailer(params, sent_at = Time.now)
-      recipients    "support@cube2media.com"
+      recipients    "larry@cube2media.com"
       from          "Cube2 Contact Mailer"
       subject       "Cube2 Contact Submission"
       body          :contact => params, :sent_on => sent_at
   end
   
   def project_mailer(params, sent_at = Time.now)
-      recipients    "support@cube2media.com"
+      recipients    "larry@cube2media.com"
       from          "Cube2 Project Mailer"
       subject       "Cube2 Project Submission"
       body          :project => params, :sent_on => sent_at
